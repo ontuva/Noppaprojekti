e7f0f19598798b62c4c857995dd4a1b616298a12
ontuva <aimokainen@hotmail.com>
2022-03-26 21:29:23 +0200

    updated inlineroll results
    - added check to see whether inlinerolls have SAVE or actual rolling (save only has the spell save DC, not actual rolling)
    - added check to see whether there were multiple rolls on inlinerolls (for ex. fireball 8d6)
    - fixed and switched regexr to match needs better
    - got rid of d in die results
    - switched other elementExists to rollExists to avoid confusion
    - put the other functions in comments for now, until they are updated too

--- html/Noppaprojekti/getData.js
+++ html/Noppaprojekti/getData.js
@@ -9,26 +9,27 @@ var die = ""; //which die was used
 var result =""; //roll result
 var elementExists = false;
 
+
 //get all the elements with roll results
 var rolls = document.getElementsByClassName("rollresult");
 
 //iterate through
 for (i = 0; i < rolls.length; i++){
 
-    //is there by element (sometimes roll20 doesn't add it), but there can only be one inside an element, so [0]
+    //is there "by" element (sometimes roll20 doesn't add it), but there can only be one inside an element, so [0]
     elementExists = !!rolls[i].getElementsByClassName("by")[0];
 
-    //if by element exists, so does timestamp, if they don't exits, they are made by the same player as previous
+    //if by element exists, so does timestamp, if they don't exits, they are made by the same player as previous message
     if(elementExists) {
         by = rolls[i].getElementsByClassName("by")[0].textContent;
         tstamp = rolls[i].getElementsByClassName("tstamp")[0].textContent;
     }
 
     //rollresult class exists even if there wasn't a roll, check to see if there is actual rolling involved
-    var elementExists = !!rolls[i].getElementsByClassName("didroll")[0];
+    var rollExists = !!rolls[i].getElementsByClassName("didroll")[0];
 
     //we only want to save rolls if we know it is an actual roll
-    if(elementExists) {
+    if(rollExists) {
 
         var dieRolls = rolls[i].getElementsByClassName("didroll");
         
@@ -40,7 +41,8 @@ for (i = 0; i < rolls.length; i++){
             
             //get what die was used, it's the classname after diceroll
             var dieName = rolls[i].getElementsByClassName("diceroll")[j].className.split(" ");
-            die = dieName[1];
+            //we don't want the d
+            die = dieName[1].split("d")[1];
 
             //make a nice fancy object
             results = {
@@ -56,8 +58,7 @@ for (i = 0; i < rolls.length; i++){
         }
 
     }//if
-}//for
-
+}//for rolls.length
 
 
 //next let's check all the sheet rolls
@@ -65,24 +66,16 @@ var sheetRolls = document.getElementsByClassName("inlinerollresult");
 //iterate through
 for (i = 0; i < sheetRolls.length; i++){
 
-    //if sheetRolls has title (it can be called title or original title, need to check both)
+    //if sheetRolls has title
     if (!!sheetRolls[i].title) {
-        var splitInfo = sheetRolls[i].title.split(/">"/);
-        
-        //if it has an result, it has a roll
-        if(!!(sheetRolls[i].title.match(/">(.*?)<\/span/))) {
-            
-            //TODO: some have more than one roll
-            var resultRoll = sheetRolls[i].title.match(/">(.*?)<\/span/)[1];
+        var title = sheetRolls[i].title;
 
-            //TODO make regexp prettier
-            var dieUsed = sheetRolls[i].title.match(/(?<=Rolling )(.*?)(\+|c| )/)[1];
-            var dDie = dieUsed.split("d");
-            die = "d"+dDie[1];
+        //if the title incluses [SAVE], the message is a spell DC and not a roll, we don't need that
+        if (!title.includes("[SAVE]")) {
 
             //if there is a roll, we need to find who made it and when, find the parent message
             var parent = sheetRolls[i].closest(".message");
-    
+            
             //is there by element (sometimes roll20 doesn't add it), but there can only be one inside an element, so [0]
             elementExists = !!parent.getElementsByClassName("by")[0];
 
@@ -92,38 +85,56 @@ for (i = 0; i < sheetRolls.length; i++){
                 tstamp = parent.getElementsByClassName("tstamp")[0].textContent;
             } //if
 
-            //save results
-            results = {
-                "by":by, 
-                "tstamp":tstamp, 
-                "die":die, 
-                "result":resultRoll
-            };
+            //what die was rolled (ex. Rolling 1d20...)
+            var dieUsed = title.match(/(?<=Rolling )(.*?)(\+|c|k| )/)[1];
+            var die = dieUsed.split("d")[1];
 
-            //push in array
-            rollResults.push(results);
-        } //if sheetrolls title match
-    }//if sheetrolls has a title
+            //also store how many times die was rolled, it can be an integer or ex. (1*5), stored before d, ex 2d20 or (1*2)d20
+            var dieAmount = dieUsed.split("d")[0];
+            dieArray = dieAmount.split("*");
+            if (dieArray.length > 1) {
+                dieAmount = dieAmount.match(/(?<=\*)[0-9]/)[0];
+            }
 
-    //TODO: Original-title inlinerollresults  
-    
+            //we need to store all the rolls
+            for (j = 0; j < dieAmount; j++) {
+                //the rollresults are between spans ex. <span class="basicdiceroll">11</span>
+                var resultRolls = title.match(/(?<=">)(.*?)(?=<\/span)/g);
+                for (k = 0; k < resultRolls.length; k++) {
+                    var resultRoll = resultRolls[k];
+
+                    //save results
+                    results = {
+                        "by":by, 
+                        "tstamp":tstamp, 
+                        "die":die, 
+                        "result":resultRoll
+                    };
+
+                    //push in array
+                    rollResults.push(results);
+
+                } // for ResultRolls length
+            } //for dieAmount
+        } //if !has [SAVE]
+    }//if sheetrolls has a title   
 }//for
 
+
 //all results
-getOverallResults(rollResults);
+//getOverallResults(rollResults);
 
 //by player
-getPlayerResults(rollResults, "Gilbin");
-getPlayerResults(rollResults, "Mirarin");
-getPlayerResults(rollResults, "Pihlaja");
-getPlayerResults(rollResults, "Vellamo");
-getPlayerResults(rollResults, "Kiljurn");
-getPlayerResults(rollResults, "Aranthir");
-getPlayerResults(rollResults, "Persikka");
-getPlayerResults(rollResults, "DM");
+//getPlayerResults(rollResults, "Gilbin");
+//getPlayerResults(rollResults, "Mirarin");
+//getPlayerResults(rollResults, "Pihlaja");
+//getPlayerResults(rollResults, "Vellamo");
+//getPlayerResults(rollResults, "Kiljurn");
+//getPlayerResults(rollResults, "Aranthir");
+//getPlayerResults(rollResults, "DM");
 
 //by damageDie
-getDamageDieResults(rollResults, "d8", "Kiljurn");
+//getDamageDieResults(rollResults, "d8", "Kiljurn");
 
-getMostUsedDamageDie(rollResults);
+//getMostUsedDamageDie(rollResults);
 
